0f916d694e7567a64221ac2d2651b1e7d5d60db6
Volodymyr Omelianiuk <ovf.1990@gmail.com>
2019-06-27 22:12:39 +0300

    timer is working, need to convert into Class

--- goit-js-hw-11-timer/js/index.js
+++ goit-js-hw-11-timer/js/index.js
@@ -5,67 +5,41 @@
 //   targetDate: new Date('Jul 17, 2019'),
 // });
 
-const countDownClock = (number = 100, format = 'seconds') => {
+const targetDate = new Date('Jul 17, 2019');
+
+/**
+ *
+ * @param {Object} targetDate
+ */
+
+const timer = targetDate => {
+  let countdown = setInterval(() => {
+    const secondsLeft = targetDate.getTime() - new Date();
+
+    if (secondsLeft <= 0) {
+      clearInterval(countdown);
+      return;
+    }
+
+    displayTimeLeft(secondsLeft);
+  }, 1000);
+};
+
+function displayTimeLeft(secondsLeft) {
   const d = document;
   const daysElement = d.querySelector('.days');
   const hoursElement = d.querySelector('.hours');
   const minutesElement = d.querySelector('.minutes');
   const secondsElement = d.querySelector('.seconds');
-  let countdown;
-  convertFormat(format);
 
-  function convertFormat(format) {
-    switch (format) {
-      case 'seconds':
-        return timer(number);
-      case 'minutes':
-        return timer(number * 60);
-      case 'hours':
-        return timer(number * 60 * 60);
-      case 'days':
-        return timer(number * 60 * 60 * 24);
-    }
-  }
+  daysElement.textContent = Math.floor(secondsLeft / (1000 * 60 * 60 * 24));
+  hoursElement.textContent = Math.floor(
+    (secondsLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  minutesElement.textContent = Math.floor(
+    (secondsLeft % (1000 * 60 * 60)) / (1000 * 60)
+  );
+  secondsElement.textContent = Math.floor((secondsLeft % (1000 * 60)) / 1000);
+}
 
-  const targetDate = new Date('Jul 17, 2019');
-  const time = targetDate - new Date();
-
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-  function timer(seconds) {
-    const now = Date.now();
-    const then = now + seconds * 1000;
-
-    countdown = setInterval(() => {
-      const secondsLeft = Math.round((then - Date.now()) / 1000);
-
-      if (secondsLeft <= 0) {
-        clearInterval(countdown);
-        return;
-      }
-
-      displayTimeLeft(secondsLeft);
-    }, 1000);
-  }
-
-  function displayTimeLeft(seconds) {
-    daysElement.textContent = Math.floor(seconds / 86400);
-    hoursElement.textContent = Math.floor((seconds % 86400) / 3600);
-    minutesElement.textContent = Math.floor(((seconds % 86400) % 3600) / 60);
-    secondsElement.textContent =
-      seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60;
-  }
-};
-
-/*
-  start countdown
-  enter number and format
-  days, hours, minutes or seconds
-*/
-countDownClock(20, 'days');
+timer(targetDate);
